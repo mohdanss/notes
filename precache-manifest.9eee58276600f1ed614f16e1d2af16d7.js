3ba355d509ed84ede7d14dd72316ef1f12bb7393
Mohd Ans <anstig3r@gmail.com>
2022-06-29 15:50:47 +0500

    Updates

---
+++ precache-manifest.9eee58276600f1ed614f16e1d2af16d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "254d65695e7919412c24",
+    "url": "/notes/static/js/main.254d6569.chunk.js"
+  },
+  {
+    "revision": "3967298bf72ec31b997e",
+    "url": "/notes/static/js/1.3967298b.chunk.js"
+  },
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "fbfc3cea775c7b3aec657e4d2ecda5bb",
+    "url": "/notes/index.html"
+  }
+];
